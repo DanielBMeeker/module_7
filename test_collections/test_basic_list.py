879ad63fe06b66363b70d9b5b1d8306bd6a4717b
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-19 10:35:45 -0500

    All Done

--- test_collections/test_basic_list.py
+++ test_collections/test_basic_list.py
@@ -1,3 +1,12 @@
+"""
+Program: test_basic_list.py
+Author: Daniel Meeker
+Date: 6/19/2020
+
+This program defines two functions, make_list and get_input
+for the purpose of creating a list of user input. This file
+tests the function make_list.
+"""
 import unittest
 from unittest.mock import patch
 import fun_with_collections.basic_list as basic_list
