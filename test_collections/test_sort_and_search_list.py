485ad75f44bc9c1008ac768fae912c05385d602f
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-19 13:23:16 -0500

    failing sort test

--- test_collections/test_sort_and_search_list.py
+++ test_collections/test_sort_and_search_list.py
@@ -5,6 +5,7 @@ import fun_with_collections.sort_and_search_list as sort_and_search_list
 list_searched = [0, 1, 2, 3, 4, 5]
 value_searched = 2
 invalid_value_searched = 6
+list_to_sort = [5, 3, 2, 4, 0, 1]
 
 
 class MyTestCase(unittest.TestCase):
@@ -13,6 +14,9 @@ class MyTestCase(unittest.TestCase):
 
     def test_search_not_in_list(self):
         self.assertEqual(sort_and_search_list.search_list(list_searched, invalid_value_searched), -1)
+
+    def test_sort_list(self):
+        self.assertEqual(sort_and_search_list.sort_list(list_to_sort), list_searched)
 
 
 if __name__ == '__main__':
