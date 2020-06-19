7a4ab4e55bc4c092ec2b0924ece944c90fc90560
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-19 10:19:43 -0500

    passed test

--- fun_with_collections/basic_list.py
+++ fun_with_collections/basic_list.py
@@ -1,6 +1,13 @@
 def make_list():
-    pass
+    user_input_list = []
+    for x in range(3):
+        user_input = get_input()
+        if not user_input.isnumeric():
+            raise ValueError
+        else:
+            user_input_list.append(int(user_input))
+    return user_input_list
 
 
 def get_input():
-    pass
+    return str
