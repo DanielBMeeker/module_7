4a23fcb203b814d951ae84fb651e4191a415acfa
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-19 11:36:45 -0500

    basic_list_exception initial commit

---
+++ fun_with_collections/basic_list_exception.py
@@ -0,0 +1,40 @@
+"""
+Program: basic_list_exception.py
+Author: Daniel Meeker
+Date: 6/19/2020
+
+
+"""
+
+
+def make_list():
+    """
+    This function calls the get_input function and saves the input to a list
+    :return: the completed list
+    """
+    user_input_list = []
+    for x in range(3):
+        user_input = get_input()
+        if user_input.isalpha():
+            # used 'isalpha()' rather than 'not isnumeric()' to
+            # account for the possibility of negative numbers
+            raise ValueError("Input is not a number")
+        else:
+            user_input_list.append(int(float(user_input)))
+    return user_input_list
+
+
+def get_input():
+    """
+    This function gets user input to pass to make_list
+    :return: input as a string
+    """
+    user_input = input("Please enter a number to add to the list: ")
+    return user_input
+
+
+if __name__ == '__main__':
+    try:
+        print(make_list())
+    except ValueError as error:
+        print(error)
