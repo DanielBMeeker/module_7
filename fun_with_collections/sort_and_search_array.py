a18e4f03dbbf4f64043678bda670da0561ff9725
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-21 18:54:07 -0500

    Second set of tests passed - comments and excception handling added

--- fun_with_collections/sort_and_search_array.py
+++ fun_with_collections/sort_and_search_array.py
@@ -1,12 +1,46 @@
+"""
+Program: sort_and_search_array.py
+Author: Daniel Meeker
+Date: 6/21/2020
+
+This program defines two functions to search or sort an array.
+"""
 import array as arr
 
 
 def sort_array(a_array):
-    pass
+    """
+    This function will take an array and sort it. It explicitly converts to an array
+    in case the user tries to pass in a list. It has exception handling in case there
+    is a type error.
+
+    :param a_array: An array passed in to be sorted
+    :return: returns the array after it has been sorted. Array's are mutable so returning it
+             will change the array where it was called and you don't need to create a new array
+             to hold the sorted original array.
+    """
+    try:
+        a_array = arr.array('i', a_array)  # explicitly convert to array in case it comes in as a list
+    except TypeError as e:
+        return e
+    a_list = a_array.tolist()  # now that it is explicitly an array convert back to list to use built in sort method
+    a_list.sort()
+    a_array = a_list
+    return arr.array('i', a_array)  # convert sorted list back to array and return it
 
 
 def search_array(a_array, value):
-    a_array = arr.array('i', a_array)  # explicitly convert to array in case it comes in as a list
+    """
+    This function takes an array and a value and searches to see if the value is found in the array.
+
+    :param a_array: The array to be searched
+    :param value: The value being searched for in the array
+    :return: Either the index of the first instance of the value being searched or -1 if value is not in array
+    """
+    try:
+        a_array = arr.array('i', a_array)  # explicitly convert to array in case it comes in as a list
+    except TypeError as e:
+        return e
     if value not in a_array:
         return -1
     else:
--- test_collections/test_sort_and_search_array.py
+++ test_collections/test_sort_and_search_array.py
@@ -1,3 +1,13 @@
+"""
+Program: test_sort_and_search_array.py
+Author: Daniel Meeker
+Date: 6/21/2020
+
+This program defines two functions to search or sort an array.
+This file tests the functions as well as checking to make sure
+that an error is raised if the user passes a list instead of
+an array.
+"""
 import unittest
 import array as arr
 from fun_with_collections import sort_and_search_array as ssa
@@ -5,6 +15,7 @@ array_searched = arr.array('i', [0, 1, 2, 3, 4, 5])
 value_searched = 2
 invalid_value_searched = 6
 array_to_sort = arr.array('i', [5, 3, 2, 4, 0, 1])
+array_type_error = [0, 1, 2, 3, 4, 'five']
 
 
 class MyTestCase(unittest.TestCase):
@@ -16,6 +27,9 @@ class MyTestCase(unittest.TestCase):
 
     def test_sort_list(self):
         self.assertEqual(ssa.sort_array(array_to_sort), array_searched)
+
+    def test_conversion_error(self):  # test to verify an error is thrown if there are mismatching types in the array.
+        self.assertRaises(TypeError, ssa.sort_array(array_type_error))
 
 
 if __name__ == '__main__':
