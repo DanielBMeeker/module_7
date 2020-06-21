40a7660f1fdde0057fae9de07fcc0c54f9a82f90
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-21 18:06:20 -0500

    topic 4 initial commit with tests

---
+++ fun_with_collections/sort_and_search_array.py
@@ -0,0 +1,6 @@
+def sort_array(a_array):
+    pass
+
+
+def search_array():
+    pass
---
+++ test_collections/test_sort_and_search_array.py
@@ -0,0 +1,22 @@
+import unittest
+import array as arr
+from fun_with_collections import sort_and_search_array as ssa
+array_searched = arr.array('i', [0, 1, 2, 3, 4, 5])
+value_searched = 2
+invalid_value_searched = 6
+array_to_sort = arr.array('i', [5, 3, 2, 4, 0, 1])
+
+
+class MyTestCase(unittest.TestCase):
+    def test_search_in_list(self):
+        self.assertEqual(ssa.search_array(array_searched, value_searched), 2)
+
+    def test_search_not_in_list(self):
+        self.assertEqual(ssa.search_list(array_searched, invalid_value_searched), -1)
+
+    def test_sort_list(self):
+        self.assertEqual(ssa.sort_list(array_to_sort), array_searched)
+
+
+if __name__ == '__main__':
+    unittest.main()
